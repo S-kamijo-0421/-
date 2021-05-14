449e15fbe9488816a0cb9c20357eee14fab552cd
S-kamijo-0421 <shiori0421397@gmail.com>
2021-05-14 11:34:22 +0900

    test2

---
+++ practice/js/jspractice.js
@@ -0,0 +1,58 @@
+// document.write("Hello World");
+// alert("Hello World");
+
+// console.log("Hello World" + "田中さん");
+// console.log(10+10);
+// console.log("10"+"10");
+
+// console.log(1 + 1);
+// console.log(1 - 1);
+// console.log(2 * 2);
+// console.log(10 / 3);
+// console.log(10 % 3);
+
+// let x = 1;
+// let y = 1;
+// x++; //x = x + 1
+// console.log(x);
+// y--; //y = y - 1
+// console.log(y);
+
+// let x = 7;
+// let y = x++;
+// let x2 = 7;
+// let y2 = ++x2;
+
+// console.log(x);
+// console.log(y);
+// console.log(x2)
+// console.log(y2)
+
+// console.log(x++);
+// console.log(++x);
+
+// 'use strict'
+
+// let age = 30;
+// let name = "田中太郎";
+
+// age = age + 2;
+
+// console.log(`${name}さんの年齢は${age}です`);
+
+// 'use strict'
+
+// let i = 2;
+// if (i === 1) {
+// console.log("真です");
+// }
+
+let testscore =80;
+
+if　(testscore >= 80) {
+document.write("合格です！おめでとうございます！");
+} else if (testscore >= 70){
+    document.write("惜しい！あと少し！");
+} else {
+    document.write("不合格です！もう少し勉強しましょう！")
+}
